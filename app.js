c3c34361a97cd507190118b66ccdbdc8d35a6db2
dopaming <over_ray@naver.com>
2021-01-10 04:00:10 +0900

    Local Variables in Pug

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
+
 const app = express();
 
+app.use(helmet()); // 기초보안
 app.set("view engine", "pug") // 기본 디렉토리는 /view
 app.use(cookieParser()); // 쿠키 미들웨어 use는 전역적용
 app.use(bodyParser.json()); // form데이터를 서버로 받아와서 활용
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet()); // 기초보안
 app.use(morgan("dev")); // 로그
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
