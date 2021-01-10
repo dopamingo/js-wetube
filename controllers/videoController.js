648da0b143a7d0ef38d829d2602661ba5bb4de5a
dopaming <over_ray@naver.com>
2021-01-10 23:54:18 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,21 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" }); //render의 첫번째 인자는 템플릿, 두번째 인자는 담을 객체
-export const search = (req, res) => res.render("search", { pageTitle: "Search" });
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
-export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detial" });
-export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const home = (req, res) =>
+    res.render("home", { pageTitle: "Home" }); //render의 첫번째 인자는 템플릿, 두번째 인자는 담을 객체
+
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const upload = (req, res) =>
+    res.render("upload", { pageTitle: "Upload" });
+
+export const videoDetail = (req, res) =>
+    res.render("videoDetail", { pageTitle: "Video Detial" });
+
+export const editVideo = (req, res) =>
+    res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) =>
+    res.render("deleteVideo", { pageTitle: "Delete Video" });
