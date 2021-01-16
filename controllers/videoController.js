38ff9674e0fe7cf9a8ab5772e14a16896e24cfac
dopaming <over_ray@naver.com>
2021-01-17 00:50:30 +0900

    Getting Video by ID

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -34,8 +34,17 @@ export const postUpload = async (req, res) => {
     res.redirect(routes.videoDetail(newVideo.id));
   };
 
-export const videoDetail = (req, res) =>
-    res.render("videoDetail", { pageTitle: "Video Detial" });
+export const videoDetail = async(req, res) => {
+    const {
+        params: { id }
+    } = req;
+    try {
+        const video = await Video.findById(id);
+        res.render("videoDetail", { pageTitle: "Video Detial", video });
+    } catch(error) {
+        res.redirect(route.home);
+    }
+};
 
 export const editVideo = (req, res) =>
     res.render("editVideo", { pageTitle: "Edit Video" });
