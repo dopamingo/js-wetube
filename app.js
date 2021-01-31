79f1577d51b222bfcde00e871cd5d9af70a37d06
dopaming <over_ray@naver.com>
2021-02-01 03:48:09 +0900

    MongoStore and Middlewares

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session"
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
@@ -14,6 +16,8 @@ import "./passport";
 
 const app = express();
 
+const CokieStore = MongoStore(session);
+
 app.use(helmet()); // 기초보안
 app.use(function(req, res, next) {
     res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
@@ -22,7 +26,6 @@ app.use(function(req, res, next) {
 app.set("view engine", "pug") // 기본 디렉토리는 /view
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
-app.use(cookieParser()); // 쿠키 미들웨어 use는 전역적용
 app.use(bodyParser.json()); // form데이터를 서버로 받아와서 활용
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev")); // 로그
@@ -30,11 +33,14 @@ app.use(
     session({
         secret: process.env.COOKIE_SECRET,
         resave: true,
-        saveUninitialized: false
+        saveUninitialized: false,
+        store: new CokieStore({ mongooseConnection: mongoose.connection })
        })
   );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cookieParser()); // 쿠키 미들웨어 use는 전역적용
+
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
