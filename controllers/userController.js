996c4cf050d38f9944a2e9ed23af55377282cfab
dopaming <over_ray@naver.com>
2021-02-14 00:28:27 +0900

    Edit User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -120,5 +120,24 @@ export const userDetail = async (req, res) => {
         res.redirect(routes.home);
     }
 };
-export const editProfile = (req, res) => res.render("editProfile");
+export const getEditProfile = (req, res) => 
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+    const {
+        body: { name, email },
+        file
+    } = req;
+    try {
+      await User.findByIdAndUpdate(req.user.id, {
+        name,
+        email,
+        avatarUrl: file ? file.path : req.user.avatarUrl // 파일 새로업로드 해서 있으면 파일경로, 없으면 기존프사
+      });
+      res.redirect(routes.me);
+    } catch (error) {
+      res.render("editProfile", { pageTitle: "Edit Profile" });
+    }
+  };
+
 export const changePassword = (req, res) => res.render("changePassword");
